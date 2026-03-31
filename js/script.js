9c24cb219f5161cffb94daf9b45025698284c77e
X <X@x.com>
2026-03-31 20:37:26 -0300

    function salvaLivro

--- js/script.js
+++ js/script.js
@@ -23,15 +23,36 @@ async function buscarLivro(){
             return
         }
         //mostrando os dados obtidos
+        
+        const titulo = data[isbnNumber].title
+        const capa = data[isbnNumber].cover.medium
+        const paginas = data[isbnNumber].number_of_pages    
+       
         preview.innerHTML = `<div class='book-card'>
         <img src='${data[isbnNumber].cover.medium}'>
             <div>
                <h3>${data[isbnNumber].title}</h3>
                <p>Nr Páginas: ${data[isbnNumber].number_of_pages}</p>
+                <button onclick="salvarLivro('${titulo}','${capa}','${paginas}','${isbn}')">Adicionar à estante</button>        
             </div>
         </div>
         `
     } catch(error) {
         alert('Erro na busca:', error)
     }
+}
+
+function salvarLivro(titulo, capa, paginas, isbn){
+    const estante = JSON.parse(localStorage.getItem('estante')) || []
+    //pegamos o array e adicionamos no fim o novo registro
+    estante.push({titulo, capa, paginas, isbn})
+    //salvamos no localStorage
+    localStorage.setItem('estante', JSON.stringify(estante))
+    Swal.fire({
+            title: 'Tudo ok!',
+            text: 'Livro salvo com sucesso!',
+            icon: 'success', //error, success, info
+            showConfirmButton: false,
+            timer: 1500 //tempo em ms
+        })
 }
